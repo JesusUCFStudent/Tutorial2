ae2ba088e403a122aa7e02608e8e84929e46a1da
Jesus Leal <jesus.leal@knights.ucf.edu>
2022-10-18 16:30:02 -0400

    Challenge commit

--- Assets/Scripts/PlayerScript.cs
+++ Assets/Scripts/PlayerScript.cs
@@ -11,21 +11,88 @@ public class PlayerScript : MonoBehaviour
 
     public Text score;
 
+    public Text lives;
+
+    public GameObject winTextObject;
+
+    public GameObject loseTextObject;
+
     private int scoreValue = 0;
+
+    private int livesValue = 3;
+
+    public AudioSource musicSource;
+
+    public AudioClip musicClipOne;
+
+    public AudioClip musicClipTwo;
+
+    Animator anim;
+
+    private bool facingRight = true;
+
+    private bool isOnGround;
+
+    public Transform groundcheck;
+
+    public float checkRadius;
+
+    public LayerMask allGround;
+
+    public float jumpForce;
 
     // Start is called before the first frame update
     void Start()
     {
         rd2d = GetComponent<Rigidbody2D>();
-        score.text = scoreValue.ToString();
-    }
+        score.text = "Score: " + scoreValue.ToString();
+        lives.text = "Lives: " + livesValue.ToString();
+        winTextObject.SetActive(false);
+        loseTextObject.SetActive(false);
 
+        if (scoreValue != 8)
+        {
+            musicSource.clip = musicClipOne;
+            musicSource.Play();
+            musicSource.loop = true;
+        }
+
+        anim = GetComponent<Animator>();
+    }
+    
+    void Update()
+    {
+        if(Input.GetKeyDown(KeyCode.Escape))
+        {
+            Application.Quit();
+        }
+    }
     // Update is called once per frame
     void FixedUpdate()
     {
         float hozMovement = Input.GetAxis("Horizontal");
         float vertMovement = Input.GetAxis("Vertical");
         rd2d.AddForce(new Vector2(hozMovement * speed, vertMovement * speed));
+        isOnGround = Physics2D.OverlapCircle(groundcheck.position, checkRadius, allGround);
+        
+        if(facingRight == false && hozMovement > 0)
+        {
+            Flip();
+        }
+
+        else if (facingRight == true && hozMovement < 0)
+        {
+            Flip();
+        }
+
+        if (Input.GetAxis("Horizontal") > 0 || Input.GetAxis("Horizontal") < 0)
+        {
+            anim.SetInteger("State", 1);
+        }
+        else
+        {
+            anim.SetInteger("State", 0);
+        }
     }
 
     private void OnCollisionEnter2D(Collision2D collision)
@@ -33,20 +100,67 @@ public class PlayerScript : MonoBehaviour
        if (collision.collider.tag == "Coin")
         {
             scoreValue += 1;
-            score.text = scoreValue.ToString();
+            score.text = "Score: " + scoreValue.ToString();
             Destroy(collision.collider.gameObject);
         }
 
+        if (collision.collider.tag == "Coin")
+        {
+            if (scoreValue == 4) 
+            {
+                transform.position = new Vector2(80.0f, 1.0f);
+                livesValue = 3;
+                lives.text = "Lives: " + livesValue.ToString();
+            }
+        }
+         
+        if (scoreValue >= 8)
+        {
+            winTextObject.SetActive(true);
+            Destroy(this);
+            musicSource.Stop();
+            musicSource.loop = false;
+            musicSource.clip = musicClipTwo;
+            musicSource.Play();
+        }
+
+        if (collision.collider.tag == "Enemy")
+        {
+            livesValue -= 1;
+            lives.text = "Lives: " + livesValue.ToString();
+            Destroy(collision.collider.gameObject);
+        }
+
+        if (livesValue <= 0)
+        {
+            loseTextObject.SetActive(true);
+            Destroy(gameObject);
+        }
     }
 
     private void OnCollisionStay2D(Collision2D collision)
     {
-        if (collision.collider.tag == "Ground")
+        if (collision.collider.tag == "Ground" && isOnGround)
         {
             if (Input.GetKey(KeyCode.W))
             {
-                rd2d.AddForce(new Vector2(0, 3), ForceMode2D.Impulse); //the 3 in this line of code is the player's "jumpforce," and you change that number to get different jump behaviors.  You can also create a public variable for it and then edit it in the inspector.
+                rd2d.AddForce(new Vector2(0, jumpForce), ForceMode2D.Impulse); //the 3 in this line of code is the player's "jumpforce," and you change that number to get different jump behaviors.  You can also create a public variable for it and then edit it in the inspector.
+                if (Input.GetAxis("Vertical") > 0 || Input.GetAxis("Vertical") < 0)
+                {
+                  anim.SetInteger("State", 2);
+                }
+                else
+                {
+                  anim.SetInteger("State", 0);
+                }
             }
         }
+    }
+    void Flip()
+    {
+        facingRight = !facingRight;
+        Vector2 Scaler = transform.localScale;
+        Scaler.x = Scaler.x * -1;
+        transform.localScale = Scaler;
     }
 }
